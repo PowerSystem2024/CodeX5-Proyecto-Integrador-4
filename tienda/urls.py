ac293868f86f49aadab001266d2fbe23b3c39774
Santiago Calzolari <santyxd321@gmail.com>
2025-10-25 18:28:30 -0300

    modifico tienda/urls y agrego logo.gif

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,16 +1,20 @@
-# ================================
-# Importar Django y vistas locales
-# ================================
-from django.urls import path
-from . import views
+# Importa las configuraciones definidas en settings.py
+from django.conf import settings
 
-# ================================
-# URLs de la app 'productos'
-# ================================
+# Importa una función que permite servir archivos estáticos (imágenes, PDFs, etc.)
+from django.conf.urls.static import static
+
+# Importa la interfaz de administración de Django
+from django.contrib import admin
+
+# Importa funciones para definir las rutas (URLs) del proyecto
+from django.urls import path, include
+
+
 urlpatterns = [
-    # Página principal: lista de productos
-    path('', views.lista_productos, name='lista_productos'),
-
-      # Detalle de producto
-    path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto')
+    path('admin/', admin.site.urls),           # Panel de administración
+    path('', include('productos.urls')),       # Rutas principales de la app productos
 ]
+
+# Permite mostrar archivos multimedia (imágenes, PDFs, etc.)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
